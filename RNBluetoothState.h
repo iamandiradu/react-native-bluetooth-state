f05c996740cc0468d660bdb093bb19e96c37d2a9
Radu I <radu.iamandi@itnt.ro>
2019-06-11 15:41:25 +0300

    update when this is fixed
    https://github.com/frostney/react-native-bluetooth-state/issues/11

--- RNBluetoothState.h
+++ RNBluetoothState.h
@@ -1,4 +1,9 @@
+/* TODO: update when this is fixed: https://github.com/frostney/react-native-bluetooth-state/issues/11 */
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#else
 #import "RCTBridgeModule.h"
+#endif
 
 @interface RNBluetoothState : NSObject <RCTBridgeModule>
 
